dc3e44181ee6ea4e02fabaf898d0ffd42c7d83e2
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-14 09:48:33 -0300

    14/3/2023-09:48

---
+++ javascriptbase.js
@@ -0,0 +1,6 @@
+//document.write ("hola muchacho")
+function sumaNumero(){
+    let num1=5;
+    let num2=19;
+    document.write(num1+num2);
+}
--- practicas.js
+++ practicas.js
@@ -1,4 +1,31 @@
-function(){
+function recogerDatos(){
     let nombreCliente = document.getElementById("nombre").value;
+    let apellidoCliente = document.getElementById("apellido").value;
+    let fechaNacimiento = document.getElementById("date").value;
+    let correoElectronico =document.getElementById("email").value;
+    let contraseña= document.getElementById("password").value;
+    let edadCliente= document.getElementById("edad").value;
+    let direccion = document.getElementById("direccion").value;
+    let provinciaVive= document.getElementById("provincia").value;
 
+    let Sexo = '';
+
+    let sexoHombre = document.getElementById("hombre");
+    let sexoMujer = document.getElementById("mujer");
+    let sexoOtros = document.getElementById("otros");
+
+    if(sexoHombre.checked ){
+        Sexo= sexoHombre.value;
+    }
+    else if(sexoMujer.checked){
+        Sexo= sexoMujer.value
+    }else{
+        Sexo= sexoOtros.value
+    }
+
+
+    let mensajefinal=  nombreCliente +  apellidoCliente + fechaNacimiento +
+     correoElectronico + contraseña + edadCliente + direccion + provinciaVive 
+
+   console.log(mensajefinal);
 }
