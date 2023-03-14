d3b2a436f2c81b17ad466f60023b5b4c9706b793
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-14 09:58:50 -0300

    14/3/2023-09:58

--- javascriptbase.js
+++ javascriptbase.js
@@ -1,6 +1,21 @@
 //document.write ("hola muchacho")
-function sumaNumero(){
+/*function sumaNumero(){
     let num1=5;
     let num2=19;
     document.write(num1+num2);
+}*/
+
+function nuestraOpcion(){
+
+    var opciones = document.getElementsByName("eleccion");
+
+    for(let i=0;i<opciones.length;i++ ){
+        console.log(opciones[i]);
+        if(opciones[i].checked){
+alert(opciones[i.value]);
+        }
+    }
+
+
+
 }
