0f999a9ff8c0fd8c278fdacb162edcc70c7cdb48
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-16 08:13:44 -0300

    16/3/2023-8:13

--- javascriptbase.js
+++ javascriptbase.js
@@ -4,7 +4,7 @@
     let num2=19;
     document.write(num1+num2);
 }*/
-
+/*
 function nuestraOpcion(){
 
     var opciones = document.getElementsByName("eleccion");
@@ -18,4 +18,80 @@ alert(opciones[i.value]);
 
 
 
-}
+}*/
+
+/*
+numero1="53";
+numero2=6;
+frase=
+document.write(frase)*/
+
+
+nombre="juan ignacio";
+
+//frase="soy "+ nombre + " y estoy caminando";
+//frase= `soy ${nombre} y estoy caminando`;
+//console.log(frase);
+
+/*htmlbackstick =`<div>
+<h1>titulo</h1>
+<h2>subtitulos</h2></div>`;
+document.write(htmlbackstick);*/
+
+
+//nombre="lucas dalto";
+
+//frase ="mi nombre es 'lucas' y soy crack";
+
+/*let numero1=23;
+let numero2=13;
+document.write(numero1==numero2)
+*/
+//{retorna false}
+
+
+/*let numero1=23;
+let numero2=13;
+document.write(numero1!=numero2)
+*/
+//{retorna true}
+
+/*let numero1=23;
+let numero2=13;
+texto1="texto 1";
+texto2="texto2";
+
+
+document.write(texto1!=texto2)
+*/
+//{retorna true}
+
+/*let numero1=23;
+let numero2=13;
+texto1="23";
+texto2="texto2";
+
+
+document.write(texto1==numero1)
+*/
+//{retorna true}
+
+/*let numero1=23;
+let numero2=13;
+texto1="23";
+texto2="texto2";
+
+
+document.write(texto1===numero1)
+*/
+//{retorna false}
+
+/*let numero1=23;
+let numero2=13;
+texto1="23";
+texto2="texto2";
+
+
+document.write(texto1!==numero1)
+*/
+//{retorna true}
